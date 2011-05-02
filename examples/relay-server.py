5a0d1b8e365122286ff4543c66c62ae7abcb7fb7
Alessandro Decina <alessandro.d@gmail.com>
2011-05-02 09:39:46 +0200

    add bindings for gst-rtsp-relay

--- examples/relay-server.py
+++ examples/relay-server.py
@@ -6,27 +6,27 @@ git = os.path.join(os.getcwd(), '.git')
 if os.path.exists(git):
     sys.path.insert(0, os.getcwd())
 
+from urlparse import urlparse
 import gobject
 gobject.threads_init()
 
 import pygst
 pygst.require("0.10")
-from gst.rtspserver import Server, MediaMapping, MediaFactory
-from rtsputils import CamMediaFactory
+from gst.rtspserver import Server
+from rtsputils import RelayMediaFactory
+
+local_url = sys.argv[1]
+remote_url = sys.argv[2]
 
 server = Server()
-server.set_service("8554")
+server.set_service(str(urlparse(local_url).port))
 
-factory = CamMediaFactory()
-factory.props.video = True
-factory.props.audio = False
-factory.props.video_width = 320
-factory.props.video_height = 240
-factory.props.video_codec = "theora"
+factory = RelayMediaFactory()
+factory.props.location = remote_url
 factory.set_shared(True)
 
 mapping = server.get_media_mapping()
-mapping.add_factory("/cam", factory)
+mapping.add_factory("/relay", factory)
 
 server.attach()
 
